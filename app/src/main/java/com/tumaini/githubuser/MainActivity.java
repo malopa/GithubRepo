c4c28a5e3cf30d1d8c153704fe42193496ce3aba
quicker <malopalusajo@gmail.com>
2022-05-23 09:21:15 +0300

    add info to read me file

--- app/src/main/java/com/tumaini/githubuser/MainActivity.java
+++ app/src/main/java/com/tumaini/githubuser/MainActivity.java
@@ -44,16 +44,10 @@ public class MainActivity extends AppCompatActivity {
         searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
             @Override
             public boolean onQueryTextSubmit(String query) {
-                if(list.contains(query)){
-                    adapter.getFilter().filter(query);
-                }else{
-                    Toast.makeText(MainActivity.this, "No Match found",Toast.LENGTH_LONG).show();
-                }
                 return false;
             }
             @Override
             public boolean onQueryTextChange(String newText) {
-                adapter.getFilter().filter(newText);
                 return false;
             }
         });
